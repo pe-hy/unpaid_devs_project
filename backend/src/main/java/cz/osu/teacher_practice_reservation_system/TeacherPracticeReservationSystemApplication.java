87527b4725ada9b883403d36e8caa5d918728f68
bezouskat <bezouska.t@gmail.com>
2021-11-04 23:03:23 +0100

    spring boot project created

---
+++ backend/src/main/java/cz/osu/teacher_practice_reservation_system/TeacherPracticeReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.teacher_practice_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherPracticeReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherPracticeReservationSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cz/osu/teacher_practice_reservation_system/TeacherPracticeReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.teacher_practice_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherPracticeReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

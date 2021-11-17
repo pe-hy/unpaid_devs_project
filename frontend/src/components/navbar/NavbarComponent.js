a722d2cdd47471a1618860761fba0880a0383ff5
pe-hy <petr.hyner10@volny.cz>
2021-11-17 15:50:28 +0100

    Added LoginControlComponent.js for basic login/logout logic (changes on click) Improved Navbar

--- frontend/src/components/navbar/NavbarComponent.js
+++ frontend/src/components/navbar/NavbarComponent.js
@@ -1,31 +1,37 @@
 import Navbar from 'react-bootstrap/Navbar'
+import {Container, Nav} from "react-bootstrap";
 import React from 'react';
-import {Container, Nav, NavDropdown} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from "../../resources/logo.png";
 import "./NavbarStyles.css"
+import { BsFillHouseFill, BsPersonCircle } from "react-icons/bs";
+import LoginControlComponent from "../logincontrol/LoginControlComponent";
 
-const user = "Nepřihláśený uźivatel"
+let iconStyles = { fontSize: "2em" };
 
 class NavbarComponent extends React.Component {
     render() {
         return (
             <div>
                 <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                    <Container>
+                    <Container fluid>
                         <Navbar.Brand href="#home"><img width="150px" height="auto" className="img-responsive" src={Logo}  alt="logo" /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="me-auto">
+                                <Nav.Link className="ms-md-5">
+                                    <BsFillHouseFill style={iconStyles}/>
+                                </Nav.Link>
+                                <Nav.Link className="ms-md-5">
+                                    <BsPersonCircle style={iconStyles}/>
+                                </Nav.Link>
                             </Nav>
                             <Nav className="me-auto">
-                                <h3> Registrace studentů na praxe </h3>
+                                <h3>Registrace studentů na praxe</h3>
                             </Nav>
                             <Nav>
-                                <span className="navbar-text" >
-                                     Nepřihlášený uživatel
-                                </span>
-                                <Nav.Link eventKey={2} href="#registrace">
-                                    Registrace
+                                <Nav.Link eventKey={2} className="navbar-text">
+                                    <LoginControlComponent/>
                                 </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>

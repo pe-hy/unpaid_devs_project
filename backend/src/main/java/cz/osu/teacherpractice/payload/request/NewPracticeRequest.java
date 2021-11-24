0bc966d5bcf3d3f51a2e5a553e8985f9743db9db
bezouskat <bezouska.t@gmail.com>
2021-11-24 02:11:16 +0100

    endpoints make reservation and cancel reservation added (multiple students support)
    now multiple students can make a reservation to a single practice depending on its capacity

--- backend/src/main/java/cz/osu/teacherpractice/payload/request/NewPracticeRequest.java
+++ backend/src/main/java/cz/osu/teacherpractice/payload/request/NewPracticeRequest.java
@@ -2,6 +2,7 @@ package cz.osu.teacherpractice.payload.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,7 +12,11 @@ public class NewPracticeRequest {
     @NotNull private LocalDate date;
     @NotNull private LocalTime start;
     @NotNull private LocalTime end;
-             private String note;
-    @NotNull private Integer capacity;
+
+    private String note;
+
+    @NotNull @Min(1)
+    private Integer capacity;
+
     @NotNull private Long subjectId;
 }

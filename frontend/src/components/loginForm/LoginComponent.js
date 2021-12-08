c85b3654306bafabcfc013c8927d97b1c25b0f5e
pe-hy <petr.hyner10@volny.cz>
2021-12-08 19:42:50 +0100

    almost works

--- frontend/src/components/loginForm/LoginComponent.js
+++ frontend/src/components/loginForm/LoginComponent.js
@@ -3,64 +3,53 @@ import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import AuthService from "../../services/AuthService";
-
+import { Navigate } from "react-router-dom";
 const required = (value) => {
   if (!value) {
     return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
+        <div className="alert alert-danger" role="alert">
+          This field is required!
+        </div>
     );
   }
 };
-
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.handleLogin = this.handleLogin.bind(this);
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
-
     this.state = {
       username: "",
       password: "",
       loading: false,
       message: "",
+      redirectToLogin: false,
     };
   }
-
   onChangeUsername(e) {
     this.setState({
       username: e.target.value,
     });
   }
-
   onChangePassword(e) {
     this.setState({
       password: e.target.value,
     });
   }
-
-  nav() {
-    const navigate = this.props.navigation;
-    navigate("/studentHome");
-  }
-
   handleLogin(e) {
+    console.log('handling login')
     e.preventDefault();
-
+    //Use something like this to check renderering, but after everything is fetched from the server
     this.setState({
-      message: "",
-      loading: true,
+      redirectToLogin: true,
     });
-
+    // Uncomment this below and add proper error handling for servercall
     this.form.validateAll();
-
     if (this.checkBtn.context._errors.length === 0) {
       AuthService.login(this.state.username, this.state.password).then(
         () => {
-          this.nav();
-          window.location.reload();
+         console.log("logged in")
         },
         (error) => {
           const resMessage =
@@ -69,7 +58,6 @@ export default class Login extends Component {
               error.response.data.message) ||
             error.message ||
             error.toString();
-
           this.setState({
             loading: false,
             message: resMessage,
@@ -82,69 +70,67 @@ export default class Login extends Component {
       });
     }
   }
-
   render() {
+    //Use similar logic like this
+    if(this.state.redirectToLogin) return <Navigate to="/studentHome" />
     return (
-      <div className="col-md-12">
-        <div className="card card-container">
-          <Form
-            onSubmit={this.handleLogin}
-            ref={(c) => {
-              this.form = c;
-            }}
-          >
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <Input
-                type="text"
-                className="form-control"
-                name="username"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                validations={[required]}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <Input
-                type="password"
-                className="form-control"
-                name="password"
-                value={this.state.password}
-                onChange={this.onChangePassword}
-                validations={[required]}
-              />
-            </div>
-
-            <div className="form-group">
-              <button
-                className="btn btn-primary btn-block"
-                disabled={this.state.loading}
-              >
-                {this.state.loading && (
-                  <span className="spinner-border spinner-border-sm"></span>
-                )}
-                <span>Login</span>
-              </button>
-            </div>
-
-            {this.state.message && (
+        <div className="col-md-12">
+          <div className="card card-container">
+            <Form
+                onSubmit={this.handleLogin}
+                ref={(c) => {
+                  this.form = c;
+                }}
+            >
               <div className="form-group">
-                <div className="alert alert-danger" role="alert">
-                  {this.state.message}
-                </div>
+                <label htmlFor="username">Username</label>
+                <Input
+                    type="text"
+                    className="form-control"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    validations={[required]}
+                />
               </div>
-            )}
-            <CheckButton
-              style={{ display: "none" }}
-              ref={(c) => {
-                this.checkBtn = c;
-              }}
-            />
-          </Form>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangePassword}
+                    validations={[required]}
+                />
+              </div>
+              <div className="form-group">
+                <button
+                    className="btn btn-primary btn-block"
+                    disabled={this.state.loading}
+                >
+                  {this.state.loading && (
+                      <span className="spinner-border spinner-border-sm"></span>
+                  )}
+                  <span>Login</span>
+                </button>
+              </div>
+              {this.state.message && (
+                  <div className="form-group">
+                    <div className="alert alert-danger" role="alert">
+                      {this.state.message}
+                    </div>
+                  </div>
+              )}
+              <CheckButton
+                  style={{ display: "none" }}
+                  ref={(c) => {
+                    this.checkBtn = c;
+                  }}
+              />
+            </Form>
+          </div>
         </div>
-      </div>
     );
   }
 }

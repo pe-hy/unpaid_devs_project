e4763ce9daada67ccefde113f360f4ef40472253
JanMrogala <jan.mrogala@seznam.cz>
2022-03-19 23:04:57 -0400

    Change of registration form between teacher/student

--- frontend/src/components/registration/RegistrationComponent.js
+++ frontend/src/components/registration/RegistrationComponent.js
@@ -13,10 +13,6 @@ const { Component } = React;
 export class RegistrationComponent extends Component {
     constructor(props) {
         super(props);
-        this.handleRegister = this.handleRegister.bind(this);
-        this.onChangeOccupation = this.onChangeOccupation(this)
-        this.onChangeEmail = this.onChangeEmail.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
         this.state = {
             email: "",
             name: "",
@@ -27,12 +23,13 @@ export class RegistrationComponent extends Component {
             occupation: "student",
             message: "",
             redirectToLogin: false,
+            studentChecked: true,
         };
+        this.handleRegister = this.handleRegister.bind(this);
+        this.onChangeOccupation = this.onChangeOccupation.bind(this)
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
       }
-
-    componentDidMount() {
-        ReactDOM.findDOMNode(this.refs.name).focus();
-    }
 
     handleRegister(e) {
         e.preventDefault();
@@ -73,18 +70,135 @@ export class RegistrationComponent extends Component {
           });
       }
 
-      onChangeOccupation(e) {
-       
-        console.log("occupation")
-      }
-
       onChangePassword(e) {
         this.setState({
             password: e.target.value,
           });
       }
+      onChangeOccupation(e) {
 
+        this.setState({
+          occupation: e.target.value,
+        });
+    
+      }
     render() {
+      if(this.state.occupation == "student"){
+        return (
+          <div className={"container-registration"}>
+              <div>
+                  <p className="thick">REGISTRACE</p>
+              </div>
+
+          <section className={" card card-container form-cointainer d-flex justify-content-center mt-2"} >
+              <Form onSubmit={this.handleRegister}
+              ref={(c) => {
+                  this.form = c;
+                  }}
+              >
+                  <br/>
+                  
+                  <div className={"d-flex justify-content-around mt-2 radio-group"}>
+                      <div>
+                          <input type="radio" id="student" name="occupation" value="student" onChange={this.onChangeOccupation} defaultChecked/>
+                          <label for="student">Student</label>
+                      </div>
+                      <div>
+                          <input type="radio" id="teacher" name="occupation" value="teacher" onChange={this.onChangeOccupation}/>
+                          <label for="teacher">Učitel</label>
+                      </div>
+                  </div>
+                  <br/><br/>
+
+                  <label className={"label-setting"}>
+                      <span className={"span-label"}> <b>E-mail</b></span>
+                      <span className={"span-input"}>
+                          <input  type="email"
+                                  className="form-control"
+                                  ref="email"
+                                  defaultValue=""
+                                  required />
+                      </span>
+                  </label>
+                  <br/>
+
+                  <label className={"label-setting"}>
+                      <span className={"span-label"}><b>Jméno</b></span>
+                          <span className={"span-input"}>
+                              <input  type="text"
+                                      className="form-control"
+                                      ref="name"
+                                      defaultValue=""
+                                      required />
+                          </span>
+                  </label>
+                  <br/>
+
+
+                  <label className={"label-setting"}>
+                      <span className={"span-label"}><b>Příjmení</b></span>
+                         <span className={"span-input"}>
+                             <input  type="text"
+                                     ref="name"
+                                     className="form-control"
+                                     defaultValue=""
+                                     required />
+                         </span>
+                  </label>
+                  <br/>
+
+                  <label className={"label-setting"}>
+                      <span className={"span-label"}><b>Heslo</b></span>
+                      <span className={"span-input"}>
+                          <div className="inner-addon right-addon">
+                          <i className="glyphicon glyphicon-user icon-form">
+                <BsFillEyeFill />
+              </i>
+                          <input  type="password"
+                                  className="form-control"
+                                  ref="password"
+                                  defaultValue=""
+                                  required /></div>
+                      </span>
+                  </label>
+                  <br/>
+
+                  <label className={"label-setting"}>
+                      <span className={"span-input"}></span>
+                      <span className={"span-input"}>
+                          <input  type="password"
+                                  ref="password_again"
+                                  className="form-control"
+                                  defaultValue=""
+                                  placeholder="Heslo znovu"
+                                  required />
+                      </span>
+                  </label>
+                  <br/>
+
+
+                  <div className={"btn-align-center"}>
+                      <button className="btn button-rgstr"><b>Zaregistrovat se</b></button>
+                  </div>
+
+                  <CheckButton
+                      style={{ display: "none" }}
+                      ref={(c) => {
+                          this.checkBtn = c;
+                      }}
+                  />
+              </Form>
+
+              <br/>
+              <span className={"account-a"}>
+                  <p>Máte účet?   <a className={"login-link"} href = "login">Přihlašte se</a>
+                  </p>
+              </span>
+          </section>
+          </div>
+      );
+      }
+      else{
         return (
             <div className={"container-registration"}>
                 <div>
@@ -99,11 +213,11 @@ export class RegistrationComponent extends Component {
                     <br/>
                     <div className={"d-flex justify-content-around mt-2 radio-group"}>
                         <div>
-                            <input type="radio" id="student" name="occupation" value="student" checked onChange={this.onChangeOccupation}/>
+                            <input type="radio" id="student" name="occupation" value="student" onChange={this.onChangeOccupation} defaultChecked />
                             <label for="student">Student</label>
                         </div>
                         <div>
-                            <input type="radio" id="teacher" name="occupation" value="učitel"/>
+                            <input type="radio" id="teacher" name="occupation" value="teacher" onChange={this.onChangeOccupation}/>
                             <label for="teacher">Učitel</label>
                         </div>
                     </div>
@@ -236,5 +350,6 @@ export class RegistrationComponent extends Component {
             </div>
         );
     }
+  }
 }
 export default RegistrationComponent

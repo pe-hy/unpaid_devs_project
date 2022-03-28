9f1ca074b1b46a0c96b677b9066315980c47e4fd
petrh <petr.hyner10@volny.cz>
2022-03-28 19:51:24 -0400

    Mistake fixed

--- backend/src/main/java/cz/osu/teacherpractice/config/SecurityConfig.java
+++ backend/src/main/java/cz/osu/teacherpractice/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/exit").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http
-                .logout().logoutSuccessHandler(this.)
+                .logout()
                 .logoutUrl("/logout")
                 .deleteCookies("access_token");
 

8b5e4227854a861f91a1ea94d049f404b7d761b4
petrh <petr.hyner10@volny.cz>
2022-04-11 18:11:15 -0400

    fixed idk how lol

--- backend/src/main/java/cz/osu/teacherpractice/service/fileManagement/UploadFileController.java
+++ backend/src/main/java/cz/osu/teacherpractice/service/fileManagement/UploadFileController.java
@@ -2,10 +2,7 @@ package cz.osu.teacherpractice.service.fileManagement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
--- frontend/src/components/teacherPersonalPage/FileManagementComponent.js
+++ frontend/src/components/teacherPersonalPage/FileManagementComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const FileManagementComponent = () => {
     const [files, setFiles] = useState('');
@@ -10,7 +10,6 @@ const FileManagementComponent = () => {
     const [fileUploadResponse, setFileUploadResponse] = useState(null);
     //base end point url
     const FILE_UPLOAD_BASE_ENDPOINT = "http://localhost:8080";
-
 
     const uploadFileHandler = (event) => {
         setFiles(event.target.files);
@@ -26,7 +25,7 @@ const FileManagementComponent = () => {
         const formData = new FormData();
 
         for (let i = 0; i < files.length; i++) {
-            if (files[i].size > 10240){
+            if (files[i].size > 10240) {
                 setFileSize(false);
                 setFileUploadProgress(false);
                 setFileUploadResponse(null);
@@ -36,49 +35,36 @@ const FileManagementComponent = () => {
             formData.append(`files`, files[i])
         }
 
-        var axios = require('axios');
-var data = formData;
+        const axios = require('axios');
+        const data = formData;
 
-var config = {
-  method: 'post',
-  url: 'http://localhost:8080/teacher/upload',
-  headers: { 
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0', 
-    'Accept': 'application/json, text/plain, */*', 
-    'Accept-Language': 'cs,sk;q=0.8,en-US;q=0.5,en;q=0.3', 
-    'Accept-Encoding': 'gzip, deflate', 
-    'Content-Type': 'application/json', 
-    'Origin': 'http://localhost:3000', 
-    'Connection': 'keep-alive', 
-    'Referer': 'http://localhost:3000/', 
-    'Sec-Fetch-Dest': 'empty', 
-    'Sec-Fetch-Mode': 'cors', 
-    'Sec-Fetch-Site': 'same-site', 
-    'Cookie': 'access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrYXJlbC5zdm9ib2RhQGVtYWlsLmN6Iiwicm9sZSI6IlJPTEVfVEVBQ0hFUiIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MC9sb2dpbiIsImV4cCI6MTY1MDg1OTIwMH0.H-GQi5cCucfPGHxS7GeYWsd6VuFFBatUfU20TQunH5c', 
-    ...data.getHeaders()
-  },
-  data : data
-};
+        const config = {
+            method: 'POST',
+            url: 'http://localhost:8080/teacher/upload',
+            withCredentials: true,
+            headers: {'content-type': 'application/json'},
+            data: data
+        };
 
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
 
     };
 
-    return(
+    return (
 
-        <form onSubmit={fileSubmitHandler}>
-            <input type="file"  multiple onChange={uploadFileHandler}/>
-            <button type='submit'>Upload</button>
-            {!fileSize && <p style={{color:'red'}}>File size exceeded!!</p>}
-            {fileUploadProgress && <p style={{color:'red'}}>Uploading File(s)</p>}
-            {fileUploadResponse!=null && <p style={{color:'green'}}>{fileUploadResponse}</p>}
-        </form>
+        <div>
+            <input type="file" multiple onChange={uploadFileHandler}/>
+            <button onClick={fileSubmitHandler}>Upload</button>
+            {!fileSize && <p style={{color: 'red'}}>File size exceeded!!</p>}
+            {fileUploadProgress && <p style={{color: 'red'}}>Uploading File(s)</p>}
+            {fileUploadResponse != null && <p style={{color: 'green'}}>{fileUploadResponse}</p>}
+        </div>
 
     );
 }

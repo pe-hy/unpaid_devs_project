094622801e652fcdd59ee2b1c03dfabfef9f5db9
JanMrogala <jan.mrogala@seznam.cz>
2022-05-09 05:29:54 -0400

    redux import fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import store from './redux/store';
+import store from './redux/store.js';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(

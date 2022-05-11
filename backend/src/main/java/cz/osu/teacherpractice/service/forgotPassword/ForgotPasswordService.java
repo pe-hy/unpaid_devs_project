31dbe0b4f63e68daaec4bdac0bc3f2d3e1b494fa
petrh <petr.hyner10@volny.cz>
2022-05-10 22:45:23 -0400

    query for students by practice

--- backend/src/main/java/cz/osu/teacherpractice/service/forgotPassword/ForgotPasswordService.java
+++ backend/src/main/java/cz/osu/teacherpractice/service/forgotPassword/ForgotPasswordService.java
@@ -42,7 +42,7 @@ public class ForgotPasswordService {
                 "                  \n" +
                 "                    </td>\n" +
                 "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Potvrďte svou registraci</span>\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Zapomenuté heslo</span>\n" +
                 "                    </td>\n" +
                 "                  </tr>\n" +
                 "                </tbody></table>\n" +

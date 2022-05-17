b2e047a57a9c663fbb468e1eb03e2fb9b2214733
JanMrogala <jan.mrogala@seznam.cz>
2022-05-17 17:48:29 -0400

    coordinator phone number validation

--- frontend/src/components/CoordinatorRole/CoordinatorPersonalPage/CoordinatorPersonalPageComponent.js
+++ frontend/src/components/CoordinatorRole/CoordinatorPersonalPage/CoordinatorPersonalPageComponent.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {axios} from "../../../axios";
+import React, { useEffect, useState } from 'react';
+import { axios } from "../../../axios";
 import "./CoordinatorPersonalPageComponent.css";
-import {BsAt, BsExclamationTriangleFill, BsFillPersonFill, BsPhone, BsTools} from "react-icons/bs";
+import { BsAt, BsExclamationTriangleFill, BsFillPersonFill, BsPhone, BsTools } from "react-icons/bs";
 import ChangePasswordComponent from "../../UnspecifiedRoles/changePassword/ChangePasswordComponent";
 
 const URL = `${process.env.REACT_APP_AXIOS_URL}`;
@@ -9,11 +9,12 @@ const POST_PHONE_EDIT = `${URL}/coordinator/changePhoneNumber`;
 const GET_DATA_URL = `${URL}/user/data`;
 
 const CoordinatorPersonalPageComponent = () => {
-    let iconStyles = {fontSize: "1.35em", marginRight: "10px"};
+    let iconStyles = { fontSize: "1.35em", marginRight: "10px" };
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [phoneNew, setPhoneNew] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const getUserData = async () => {
         // Make first two requests
         const response = await Promise.all([
@@ -38,7 +39,7 @@ const CoordinatorPersonalPageComponent = () => {
         if (!validatePhoneNum(value)) {
             return (
                 <div className="alert alert-danger my-alert text-bold" role="alert">
-                    <BsExclamationTriangleFill/> Špatný formát telefonního čísla!
+                    <BsExclamationTriangleFill /> Špatný formát telefonního čísla!
                 </div>
             );
         }
@@ -58,8 +59,12 @@ const CoordinatorPersonalPageComponent = () => {
 
 
     const editPhone = async () => {
+        if (!validatePhoneNum(phoneNew)) {
+            setErrorMessage("Špatný formát telefonního čísla!")
+            return;
+        }
         const response = await axios({
-            headers: {'content-type': 'application/json'},
+            headers: { 'content-type': 'application/json' },
             url: POST_PHONE_EDIT,
             withCredentials: true,
             method: "POST",
@@ -69,20 +74,21 @@ const CoordinatorPersonalPageComponent = () => {
             console.log(err.response.data.message);
         });
         if (response && response.data) {
+            setErrorMessage("");
             getUserData();
         }
     };
 
     return (
-        <div style={{marginTop: "30px"}}>
+        <div style={{ marginTop: "30px" }}>
             <div className="row">
                 <div className="col-sm">
                     <h1>Osobní stránka</h1>
-                    <p style={{paddingTop: "25px"}}><BsFillPersonFill style={iconStyles}/><b>Jméno:</b> {name}</p>
-                    <p><b><BsAt style={iconStyles}/>E-mail:</b> {email}</p>
+                    <p style={{ paddingTop: "25px" }}><BsFillPersonFill style={iconStyles} /><b>Jméno:</b> {name}</p>
+                    <p><b><BsAt style={iconStyles} />E-mail:</b> {email}</p>
                     <div className="container">
                         <div className="row">
-                            <div className="col-sm-4"><b><BsPhone style={iconStyles}/>Telefon</b>: {phone}</div>
+                            <div className="col-sm-4"><b><BsPhone style={iconStyles} />Telefon</b>: {phone}</div>
                             <form onSubmit={(e) => {
                                 e.preventDefault();
                                 editPhone();
@@ -90,21 +96,24 @@ const CoordinatorPersonalPageComponent = () => {
                                 <div className="col-sm-4 mt-3">
                                     <div className="form-group">
                                         <input type="text" className="form-control" id="phoneNew"
-                                               placeholder="Telefon"
+                                            placeholder="Telefon"
 
-                                               onChange={(e) => {
+                                            onChange={(e) => {
 
-                                                   setPhoneNew(e.target.value);
-                                               }}/>
+                                                setPhoneNew(e.target.value);
+                                            }} />
                                     </div>
                                 </div>
+                                {errorMessage && <div className="alert alert-danger my-alert text-bold" role="alert">
+                                <BsExclamationTriangleFill /> Špatný formát telefonního čísla!
+                            </div>}
                                 <div className="col-sm-4 mt-3 mb-3">
                                     <button type="submit" className="change-btn">Změnit telefon</button>
                                 </div>
                             </form>
                         </div>
                     </div>
-                    <p><b><BsTools style={iconStyles}/>Změna hesla: <ChangePasswordComponent/></b></p>
+                    <p><b><BsTools style={iconStyles} />Změna hesla: <ChangePasswordComponent /></b></p>
                 </div>
                 <div className="col-sm uploadCol"></div>
             </div>
